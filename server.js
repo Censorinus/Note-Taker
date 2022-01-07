e84f6504fc6d2234b9f9d4f21d761c0678932871
Censorinus <jgrant.2901@gmail.com>
2022-01-07 16:24:41 -0500

    Final commit

---
+++ server.js
@@ -0,0 +1,21 @@
+//This file contains all of the server implementation for the express server
+
+//Include the express module and the routes
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+const app = express();
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+//Listen on the defined port
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
